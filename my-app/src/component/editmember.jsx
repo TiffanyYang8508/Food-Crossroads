0e301d8197870075c2cf1f8dba8399db7aab9e5b
shinamon <jammy83091@gmail.com>
2022-02-20 12:54:01 +0800

    訂位後台

--- my-app/src/component/editmember.jsx
+++ my-app/src/component/editmember.jsx
@@ -46,190 +46,137 @@ class EditMember extends Component {
   render() {
     return (
       <React.Fragment>
-        <div class="header_page">
+        <div className="header_page">
           <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <a class="navbar-brand" href="index.html">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <a className="navbar-brand" href="index.html">
                 <img src={require("../style/img/logo.png")} alt="LOGO" />
               </a>
-              <div class="nav_icon">
-                <a class="nav_a" href="list.html">
-                  <i class="fas fa-store"></i>餐廳列表
+              <div className="nav_icon">
+                <a className="nav_a" href="list.html">
+                  <i className="fas fa-store"></i>餐廳列表
                 </a>
-                <a class="nav_a" href="shoppingchar.html">
-                  <i class="fas fa-shopping-cart"></i>購物車
+                <a className="nav_a" href="shoppingchar.html">
+                  <i className="fas fa-shopping-cart"></i>購物車
                 </a>
-                <a class="nav_a" href="member_login.html">
-                  <i class="fas fa-user-circle"></i>會員登入
+                <a className="nav_a" href="member_login.html">
+                  <i className="fas fa-user-circle"></i>會員登入
                 </a>
               </div>
-              <div class="rwd_icon">
-                <a class="nav_a" href="list.html">
-                  <i class="fas fa-store"></i>
+              <div className="rwd_icon">
+                <a className="nav_a" href="list.html">
+                  <i className="fas fa-store"></i>
                 </a>
-                <a class="nav_a" href="shoppingchar.html">
-                  <i class="fas fa-shopping-cart"></i>
+                <a className="nav_a" href="shoppingchar.html">
+                  <i className="fas fa-shopping-cart"></i>
                 </a>
-                <a class="nav_a" href="member_login.html">
-                  <i class="fas fa-user-circle"></i>
+                <a className="nav_a" href="member_login.html">
+                  <i className="fas fa-user-circle"></i>
                 </a>
               </div>
             </nav>
           </header>
         </div>
 
-        <aside class="aside_menu">
-          <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a href="member.html">會員資料</a>
-                </h4>
-              </div>
+        <aside className="aside_menu">
+        <div className="panel-group" id="accordion">
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title"><a href="member.html">會員資料</a>
+                    </h4>
+                </div>
             </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a href="booking_manage.html">訂位管理</a>
-                </h4>
-              </div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title"><a href="booking_manage.html">訂位管理</a>
+                    </h4>
+                </div>
             </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a href="order_manage.html">訂餐管理</a>
-                </h4>
-              </div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title"><a href="order_manage.html">訂餐管理</a>
+                    </h4>
+                </div>
             </div>
-          </div>
-        </aside>
-
-        <input type="checkbox" name="aside_menu_chk" id="aside_menu_chk" />
-        <div class="mobile_aside_menu">
-          <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a href="member.html">會員資料</a>
-                </h4>
-              </div>
-            </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a href="booking_manage.html">訂位管理</a>
-                </h4>
-              </div>
-            </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a href="order_manage.html">訂餐管理</a>
-                </h4>
-              </div>
-            </div>
-          </div>
-          <label for="aside_menu_chk">
-            <i class="fas fa-angle-right"></i>
-          </label>
         </div>
+    </aside>
 
-        <section id="edit_member">
-          <div class="container-fluid default_container member_div">
-            <div class="row">
-              <div class="col-md-12">
-                <form>
-                  <div>
-                    <p class="title_member_edit">會員資料修改</p>
-                  </div>
-                  <div class="form-group form-inline">
-                    <label for="name" class="edit_label">
-                      姓　　名:
-                    </label>
-                    <input
-                      type="text"
-                      class="form-control edit_text"
-                      id="name"
-                      value={this.state.Member.user_name}
-                      name="name"
-                      onChange={this.NameChange}
-                      required
-                    />
-                  </div>
-                  <div class="form-group form-inline">
-                    <label for="email" class="edit_label">
-                      信　　箱:
-                    </label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="email"
-                      value="email@gmail.com"
-                      name="email"
-                      disabled
-                    />
-                  </div>
-                  <div class="form-group form-inline">
-                    <label for="pwd" class="edit_label">
-                      密　　碼:
-                    </label>
-                    <input
-                      type="password"
-                      class="form-control"
-                      id="pwd"
-                      value={this.state.Member.user_pwd}
-                      name="pwd"
-                      disabled
-                    />
-                    <div class="invalid-feedback">密碼</div>
-                  </div>
-
-                  <div class="form-group form-inline">
-                    <label for="tel" class="edit_label">
-                      電　　話:
-                    </label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="tel"
-                      value={this.state.Member.user_tel}
-                      onChange={this.TelChange}
-                      name="tel"
-                      required
-                    />
-                  </div>
-                  <div class="edit_btn_div">
-                    <button
-                      type="button"
-                      onclick="confirm()"
-                      class="member_back_btn"
-                    >
-                      返回
-                    </button>
-                    <button
-                      type="button"
-                      onClick={this.okbtnClick}
-                      onclick="confirm()"
-                      class="member_confirm_btn"
-                    >
-                      確定修改
-                    </button>
-                  </div>
-                </form>
-              </div>
+    <input type="checkbox" name="aside_menu_chk" id="aside_menu_chk"/>
+    <div className="mobile_aside_menu">
+        <div className="panel-group" id="accordion">
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title"><a href="member.html">會員資料</a>
+                    </h4>
+                </div>
             </div>
-          </div>
-        </section>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title"><a href="booking_manage.html">訂位管理</a>
+                    </h4>
+                </div>
+            </div>
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h4 className="panel-title"><a href="order_manage.html">訂餐管理</a>
+                    </h4>
+                </div>
+            </div>
+        </div>
+        <label for="aside_menu_chk">
+            <i className="fas fa-angle-right"></i>
+        </label>
+    </div>
+    
+        <section id="edit_member">
+        <div className="container member_div">
+            <div className="row">
+                <div className="col-md-12">
+                    <form>
+                        <div>
+                            <p className="title_member_edit">會員資料修改</p>
+                        </div>
+                        <div className="form-group form-inline">
+                            <label for="name" className="edit_label">姓　　名:</label>
+                            <input type="text" className="form-control edit_text" id="name" value={this.state.Member.user_name} name="name" onChange={this.NameChange} required/>
+                        </div>
+                        <div className="form-group form-inline">
+                            <label for="email" className="edit_label">信　　箱:</label>
+                            <input type="email" className="form-control" id="email" value={this.state.Member.user_email}  name="email"
+                                disabled/>
+                        </div>
+                        <div className="form-group form-inline">
+                            <label for="pwd" className="edit_label">密　　碼:</label>
+                            <input type="password" className="form-control" id="pwd" value={this.state.Member.user_pwd} name="pwd" required/>
+                            <div className="invalid-feedback">請輸入密碼</div>
+                        </div>
+                        <div className="form-group form-inline">
+                            <label for="tel" className="edit_label">電　　話:</label>
+                            <input type="text" className="form-control" id="tel" value={this.state.Member.user_tel} name="tel" onChange={this.TelChange} required/>
+                        </div>
+                        <div className="edit_btn_div">
+                            <button type="button" onclick="confirm()" className="member_back_btn">
+                                返回
+                            </button>
+                            <button type="button" onclick="confirm()" className="member_confirm_btn">
+                                確定修改
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 
-        <div class="footer_page">
+        <div className="footer_page">
           <footer>
-            <div class="footer_link">
+            <div className="footer_link">
               <a href="#">關於我們</a>
               <a href="#">商家入口</a>
               <a href="#">常見問題</a>
               <a href="#">聯絡我們</a>
             </div>
-            <div class="footer_text">
+            <div className="footer_text">
               <p>Copyright &copy; 2022 FoodCrossesRoads.All Rights Reserved.</p>
             </div>
           </footer>
