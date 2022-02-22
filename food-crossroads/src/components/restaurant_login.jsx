7f4c76d8f8e43b62a67d850208b313718831eedb
CHEN <asd72607230@gmail.com>
2022-02-22 11:33:27 +0800

    .

--- food-crossroads/src/components/restaurant_login.jsx
+++ food-crossroads/src/components/restaurant_login.jsx
@@ -7,7 +7,26 @@ import "../css/restaurant_login.css"
 
 
 class Restaurant_login extends Component {
-    state = {}
+    state = {
+        restaurant: { unified_compilcation: "test", restaurant_pwd: "***" }
+    }
+
+    inputAccount = (e) => {
+        var newState = {...this.state};
+        newState.restaurant.unified_compilcation = e.target.value;
+        this.setState(newState);
+    }
+
+    inputPwd = (e) => {
+        var newState = {...this.state};
+        newState.restaurant.restaurant_pwd = e.target.value;
+        this.setState(newState);
+    }
+
+    loginClick = (e) => {
+        console.log("loginclick")
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -16,28 +35,32 @@ class Restaurant_login extends Component {
                     <Header />
                 </header>
 
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="restaurant_login_container">
-                                <div class="restaurant_login_title">
-                                    <i class="fas fa-user-circle"></i>後台管理登入
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="restaurant_login_container">
+                                <div className="restaurant_login_title">
+                                    <i className="fas fa-user-circle"></i>後台管理登入
                                 </div>
-                                <div class="restaurant_form">
+                                <div className="restaurant_form">
                                     <form action="" method="">
-                                        <div class="form-group row">
-                                            <label for="account" class="form_account"></label>
-                                            <input type="text" id="account" class="form_account" placeholder="請輸入您的帳號" />
+                                        <div className="form-group row">
+                                            <label htmlFor="account" className="form_account"></label>
+                                            <input type="text" id="account" name="account" className="form_account" placeholder="請輸入您的帳號"
+                                                value={this.state.restaurant.unified_compilcation}
+                                                onChange={this.inputAccount} />
                                         </div>
-                                        <div class="form-group row">
-                                            <label for="secret" class="form_secret"></label>
-                                            <input type="password" id="secret" class="form_secret" placeholder="請輸入您的密碼" />
+                                        <div className="form-group row">
+                                            <label htmlFor="password" className="form_secret"></label>
+                                            <input type="password" id="password" name="password" className="form_secret" placeholder="請輸入您的密碼"
+                                                value={this.state.restaurant.restaurant_pwd}
+                                                onChange={this.inputPwd} />
                                         </div>
-                                        <div class="form-group row">
-                                            <input type="submit" class="btn_member_login" value="登入" />
+                                        <div className="form-group row">
+                                            <input type="button" onClick={this.loginClick} className="btn_member_login" value="登入" />
                                         </div>
-                                        <div class="form-group row">
-                                            <div class="span_container forgot">
+                                        <div className="form-group row">
+                                            <div className="span_container forgot">
                                                 <div><a href="#forgot_password">忘記密碼?</a></div>
                                                 <div>
                                                     <input type="checkbox" name="remember_password" id="remember_password" />
