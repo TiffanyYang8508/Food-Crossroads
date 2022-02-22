030bbe001ccc7544828150d617bbfadeb44a0756
Vicky <vicky0602760348@gmail.com>
2022-02-22 15:09:46 +0800

    0222購物車jsx新增

---
+++ food-crossroads/src/components/orderdisplay.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+
+import "../fontawesome-free-5.15.4-web/css/all.min.css";
+import "../css/style.css";
+import Header from './header';
+import Footer from './footer';
+import "../css/orderdisplay.css";
+
+
+class Orderdisplay extends Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+                <div className="container">
+                    <nav aria-label="breadcrumb">
+                        <ul className="breadcrumb">
+                            <li className="breadcrumb-item"><a href="#index"><span>首頁</span></a></li>
+                            <li className="breadcrumb-item"><a href="#restaurantList"><span>餐廳列表</span></a></li>
+                            <li className="breadcrumb-item active" aria-current="page"><span>訂餐</span></li>
+                        </ul>
+                        <a href="#"><img src="./img/people_plus@3x.png" className="rwd_icon_plus" /></a>
+                    </nav>
+
+                    <div className="table-responsive-md row" style={{ 'font-size': '16px', 'text-align': 'center' }}>
+                        <table className="table col-md-9" id="table_order">
+                            <thead>
+                                <tr>
+                                    <th>名稱</th>
+                                    <th>餐點名稱</th>
+                                    <th className="text-nowrap">餐點備註</th>
+                                    <th>數量</th>
+                                    <th>金額</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td scope="row">團長</td>
+                                    <td>餐點名稱<br />餐點名稱</td>
+                                    <td>小辣<br />不要蔥、不要蒜</td>
+                                    <td>1<br />1</td>
+                                    <td>50<br />50</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">團員</td>
+                                    <td>餐點名稱</td>
+                                    <td>小辣</td>
+                                    <td>1</td>
+                                    <td>50</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">團員</td>
+                                    <td>餐點名稱</td>
+                                    <td>小辣</td>
+                                    <td>1</td>
+                                    <td>50</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">團員</td>
+                                    <td>餐點名稱</td>
+                                    <td>小辣</td>
+                                    <td>1</td>
+                                    <td>50</td>
+                                </tr>
+
+                                <tr style={{ 'font-size': '18px' }}>
+                                    <td scope="row" colspan="4" style={{ 'text-align': 'right' }}><b>總金額</b></td>
+                                    <td>
+                                        NT$ 250
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <br /><br />
+
+                        <div className="share_member col-md-3 rwd_share_member" id="ordermember_Sidebar">
+
+                            <table className="table share_member_table rwd_share_member_table"
+                                style={{ 'font-size': '18px', 'width': '50px', 'line-height': '1em', 'border': '1px solid #403c3c', 'border-radius': '10%' }}>
+                                <tbody>
+                                    <tr className="close_btn">
+                                        <td style={{ 'color': '#585555c5' }}>
+                                            <i className="fas fa-times-circle fa-lg d-flex float-right"></i>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <div className="" id="order_share">
+                                                <a href="#"><img src="../img/people_plus@3x.png"
+                                                    style={{ 'width': '25px', 'height': '20px', 'margin-right': '5px' }} /></a>
+                                                <button type="button" className="btn_view" style={{ 'font-size': '18px' }}
+                                                    title="copied">揪團分享連結</button>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td><b>團長:</b></td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>總金額：＊＊＊
+                                            <br /><br />
+                                            <button type="button" className="btn_view">檢視已點清單</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="container order_checkbtn rwd_order_checkbtn d-flex flex-row-reverse">
+                            <button id="checkout" type="button" className="btn btn-danger btn-lg ">去結帳</button>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </React.Fragment>
+        );
+    }
+}
+
+export default Orderdisplay;
---
+++ food-crossroads/src/components/shoppingchar.jsx
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+
+import "../fontawesome-free-5.15.4-web/css/all.min.css";
+import "../css/style.css";
+import Header from './header';
+import Footer from './footer';
+import "../css/shoppingchar.css"
+
+
+class Shoppingchar extends Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+                <div id='shoppingchar'>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                <nav aria-label="breadcrumb">
+                                    <ul className="breadcrumb">
+                                        <li className="breadcrumb-item"><a href="index.html"><span>首頁</span></a></li>
+                                        <li className="breadcrumb-item"><a href="list.html"><span>餐廳列表</span></a></li>
+                                        <li className="breadcrumb-item active" aria-current="page"><span>訂餐</span></li>
+                                    </ul>
+                                </nav>
+
+
+                                <div id="shoppingchar">
+                                    <table className="table shopping-table">
+                                        <thead>
+                                            <tr>
+                                                <th colspan="3">商品</th>
+                                                <th>單價</th>
+                                                <th>數量</th>
+                                                <th>總計</th>
+                                                <th>刪除</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            <tr className="tr_title">
+                                                <td colspan="7">
+                                                    <h5>餐廳名稱</h5>
+                                                </td>
+                                            </tr>
+
+                                            <tr className="shopping-row" id="shopping-row">
+                                                <td>
+                                                    <input className="shopping-checkbox checkmark" id="shopping-checkbox" type="checkbox"
+                                                        name="shopping-checkbox" />
+                                                </td>
+                                                <td className="wrapper">
+                                                    <img src="img/image_shoppingchar/imge01.jpeg" className="img-fluid img-thumbnail"
+                                                        alt="" />
+                                                </td>
+                                                <td>
+                                                    <span id="ordername" className="ordername">餐點名</span>
+                                                </td>
+                                                <td>
+                                                    <span>$123</span>
+                                                </td>
+                                                <td>
+
+                                                    <span className="qty-squre">
+                                                        <input id="qtyminus" type="button" value="-" className="qty-btn" />
+                                                        <input type="text" value="1" className="qty" name="quantity" />
+                                                        <input id="qtyplus" type="button" value="+" className="qty-btn" />
+                                                    </span>
+                                                </td>
+                                                <td>
+                                                    <span>$1234</span>
+                                                </td>
+                                                <td className="delete-shopping">
+                                                    <span>刪除</span>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                                <div id="mobile_shopping">
+                                    <div className="container-fluid">
+                                        <div className="row">
+                                            <div className="col-12">
+                                                <div className="mobile_shopping_div">
+                                                    <input className="shopping-checkbox checkmark" id="shopping-checkbox" type="checkbox"
+                                                        name="shopping-checkbox" />
+                                                    <div className="mobile_shopping_img">
+                                                        <img src="img/image_shoppingchar/imge01.jpeg" className="img-fluid img-thumbnail"
+                                                            alt="" />
+                                                    </div>
+                                                    <div className="mobile_shopping_text">
+                                                        <span className="mobile_span">餐點名</span>
+                                                        <span className="mobile_span">$123</span>
+                                                        <span className="qty-squre">
+                                                            <input id="qtyminus" type="button" value="-" className="qty-btn" />
+                                                            <input type="text" value="1" className="qty" name="quantity" />
+                                                            <input id="qtyplus" type="button" value="+" className="qty-btn" />
+                                                        </span>
+                                                        <span className="mobile_span">總額:$1234</span>
+                                                    </div>
+                                                    <div className="mobile_delete">
+                                                        <button>刪除</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="all-squre" id="all-squre">
+                                    <label className="media">
+                                        <input id="shopping-checkbox" className="shopping-checkbox checkmark" name="shopping-allcheckbox"
+                                            type="checkbox" />
+                                        <span className="media-body">全選</span>
+                                    </label>
+                                    <p>
+                                        <b>總金額(3個商品)$1234</b>
+                                    </p>
+                                    <button className="buy-btn">
+                                        <b>結帳</b>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <Footer />
+            </React.Fragment >
+        );
+
+    }
+}
+
+export default Shoppingchar;
