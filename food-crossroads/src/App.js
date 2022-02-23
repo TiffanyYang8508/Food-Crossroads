0e8af6458371804a9af591f5564e72c0d3f1546a
shinamon <jammy83091@gmail.com>
2022-02-23 09:46:45 +0800

    修改會員註冊

--- food-crossroads/src/App.js
+++ food-crossroads/src/App.js
@@ -5,6 +5,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Index from "./components/Index";
 import Restaurant_list from "./components/restaurant_list";
 import Member_register from "./components/member_register";
+import Member_register_success from "./components/member_register_success";
 import Member_login from "./components/member_login";
 import Restaurant_self_pick_up from "./components/restaurant_self_pick_up";
 import Restaurant_booking_history from "./components/restaurant_booking_history";
@@ -14,24 +15,32 @@ import Member from "./components/member_memberinfo";
 import EditMember from "./components/member_editmember";
 import Restaurant_login from "./components/restaurant_login";
 
-
 class App extends Component {
   state = {};
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/"                           component={Index}                       exact/>
-          <Route path="/restaurant/list/:category?" component={Restaurant_list}             exact />
-          <Route path="/register"                   component={Member_register}             exact />
-          <Route path="/self/pick"                  component={Restaurant_self_pick_up}     exact />
-          <Route path="/booking_history"            component={Restaurant_booking_history}  exact />
-          <Route path="/member/login"               component={Member_login} />
-          <Route path="/restaurant/login"           component={Restaurant_login}            exact />
-          <Route path="/ordermanagement"            component={Ordermanagement} />
-          <Route path="/bookingmanagement"          component={BookingManagement} />
-          <Route path="/member/:id"                 component={Member} />
-          <Route path="/editmember/:id"             component={EditMember} />
+          <Route path="/" component={Index} exact />
+          <Route
+            path="/restaurant/list/:category?"
+            component={Restaurant_list}
+            exact
+          />
+          <Route path="/register" component={Member_register} exact />
+          <Route path="/register/success" component={Member_register_success} />
+          <Route path="/self/pick" component={Restaurant_self_pick_up} exact />
+          <Route
+            path="/booking_history"
+            component={Restaurant_booking_history}
+            exact
+          />
+          <Route path="/member/login" component={Member_login} />
+          <Route path="/restaurant/login" component={Restaurant_login} exact />
+          <Route path="/ordermanagement" component={Ordermanagement} />
+          <Route path="/bookingmanagement" component={BookingManagement} />
+          <Route path="/member/:id" component={Member} />
+          <Route path="/editmember/:id" component={EditMember} />
         </Switch>
       </BrowserRouter>
     );
